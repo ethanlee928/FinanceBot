40257f69ef5f66435801eb34467dca7e070516e5
ethanlee928 <ethan2000.el@gmail.com>
2023-01-10 00:20:56 +0800

    implemented on_chart, added some exceptions

--- polygon/handlers/command_handler.py
+++ polygon/handlers/command_handler.py
@@ -1,15 +1,17 @@
+import os
+
 from overrides import override
 
-from utils import CommandHandler, Command, logger
-from .candles import CandleStick, CandleSticks
+from utils import CommandHandler, Command, TimeStamp, logger
 from .clients import PolygonClient, PolygonExceptions, SlackClient
 
 
 class PolygonBotHandler(CommandHandler):
-    def __init__(self, max_workers: int = 5) -> None:
+    def __init__(self, data_dir: str, max_workers: int = 5) -> None:
         super().__init__(max_workers)
         self.polygon_client = PolygonClient()
         self.slack_client = SlackClient()
+        self.data_dir = data_dir
 
     @override
     def on_command(self, command: Command):
@@ -21,15 +23,29 @@ class PolygonBotHandler(CommandHandler):
             else:
                 logger.warning(f"{command._id} is not implemented")
         except PolygonExceptions.BadResponse:
-            # CASES: more than 5 api call / min, wrong credentials
             error_msg = ":warning: Rate limit: 5 API Calls / Minute"
-            res = self.slack_client.send_message(command.channel_id, "")
+            return self.slack_client.send_message(command.channel_id, error_msg)
+        except PolygonExceptions.NoResultsError:
+            error_msg = ":warning: No results for the request"
+            return self.slack_client.send_message(command.channel_id, error_msg)
 
     def _on_ping(self, command: Command):
         _status = ":large_green_circle:" if self.polygon_client.server_is_healthy() else ":red_circle:"
         msg = "Polygon Server Status: " + _status
-        res = self.slack_client.send_message(command.channel_id, msg)
-        logger.info(f"response: {res}")
+        return self.slack_client.send_message(command.channel_id, msg)
 
     def _on_chart(self, command: Command):
-        ...
+        tickers = command.body["tickers"]
+        candle_sticks = self.polygon_client.get_candle_sticks(
+            tickers,
+            command.body["multiplier"],
+            command.body["timespan"],
+            command.body["from"],
+            command.body["to"],
+        )
+        save_dir = f"{self.data_dir}/{tickers}/"
+        if not os.path.exists(save_dir):
+            os.mkdir(save_dir)
+        save_path = f"{save_dir}/{TimeStamp.get_ts_now(TimeStamp.DEFAULT)}.png"
+        candle_sticks.plot_graph(save_path)
+        return self.slack_client.upload_file(command.channel_id, save_path)
--- polygon/main.py
+++ polygon/main.py
@@ -1,9 +1,12 @@
-from polygon import RESTClient
-import polygon.exceptions as PolygonExceptions
 from utils import Command
 from handlers import PolygonBotHandler
 
 if __name__ == "__main__":
-    cmd = Command(Command.ID.PING, channel_id="C03GVUL4SHF", body={})
-    handler = PolygonBotHandler()
+    cmd = Command(
+        Command.ID.CHART,
+        channel_id="C03GVUL4SHF",
+        body={"tickers": "C:JPYHKD", "multiplier": 1, "timespan": "day", "from": "2022-06-01", "to": "2022-12-31"},
+    )
+
+    handler = PolygonBotHandler(data_dir="./data/")
     handler.on_command(command=cmd)
