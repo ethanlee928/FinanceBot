5a21a0c9dd2c7e4cc310daff0f533de669d5ddbf
ethanlee928 <ethan2000.el@gmail.com>
2023-01-11 01:31:43 +0800

    main for polygonbot

--- polygon/main.py
+++ polygon/main.py
@@ -1,12 +1,47 @@
-from utils import Command
+import argparse
+
+from utils import MQTTMessageHandler, Subscriber, Broker, broker_config, logger
 from handlers import PolygonBotHandler
 
-if __name__ == "__main__":
-    cmd = Command(
-        Command.ID.CHART,
-        channel_id="C03GVUL4SHF",
-        body={"tickers": "C:JPYHKD", "multiplier": 1, "timespan": "day", "from": "2022-06-01", "to": "2022-12-31"},
-    )
 
-    handler = PolygonBotHandler(data_dir="./data/")
-    handler.on_command(command=cmd)
+class PolygonBot:
+    def __init__(self, subscriber: Subscriber, data_dir: str) -> None:
+        self.subscriber = subscriber
+        self.data_dir = data_dir
+
+    def start(self) -> None:
+        handlers = [PolygonBotHandler(self.data_dir)]
+        self.subscriber.register_handlers(handlers)
+        self.subscriber.start()
+
+    def terminate(self) -> None:
+        handler: MQTTMessageHandler
+        for handler in self.subscriber.handlers:
+            handler.on_terminate()
+        self.subscriber.stop()
+
+
+def main(_args):
+    try:
+        broker = Broker.from_dict(broker_config)
+        subscriber = Subscriber(_args.client_id, broker, _args.topic)
+        app = PolygonBot(subscriber, _args.data_dir)
+        app.start()
+    except KeyboardInterrupt:
+        logger.warning("Terminating application...")
+        app.terminate()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("PolygonBot arguments")
+    parser.add_argument("--topic", type=str, default="pair")
+    parser.add_argument("--client-id", type=str, default="polygonbot")
+    parser.add_argument("--data-dir", type=str, default="./data/")
+
+    args = parser.parse_args()
+    try:
+        main(args)
+    except Exception as err:
+        logger.exception(err)
+    finally:
+        logger.warning("Exiting main...")
---
+++ polygon/tests/test_main.py
@@ -0,0 +1,29 @@
+import unittest
+
+from utils import Command, MQTTMessage, Publisher, Broker, broker_config
+
+
+class TestMain(unittest.TestCase):
+    """Psuedo test cases -> all asserts will return true
+    Acts like integration test, so main.py should be running at the same time
+    """
+
+    def setUp(self) -> None:
+        client_id = "test-publisher"
+        self.publisher = Publisher(client_id, Broker.from_dict(broker_config))
+
+    def test_on_ping_command(self):
+        cmd = Command(Command.ID.PING, "C03GVUL4SHF", body={})
+        mq_msg = MQTTMessage(topic="pair", payload=cmd.to_payload())
+        self.publisher.publish(mq_msg)
+        self.assertIsInstance(cmd, Command)
+
+    def test_on_chart_command(self):
+        cmd = Command(
+            Command.ID.CHART,
+            "C03GVUL4SHF",
+            body={"tickers": "C:JPYHKD", "multiplier": 1, "timespan": "day", "from": "2022-06-01", "to": "2022-12-31"},
+        )
+        mq_msg = MQTTMessage(topic="pair", payload=cmd.to_payload())
+        self.publisher.publish(mq_msg)
+        self.assertIsInstance(cmd, Command)
