af6f3dc10a8d0c43c28a267a875d736b86450fdf
ethanlee928 <ethan2000.el@gmail.com>
2023-01-13 00:44:50 +0800

    fixed main.py

--- slack/main.py
+++ slack/main.py
@@ -1,35 +1,43 @@
 import os
 import argparse
-from flask import Flask
+from typing import List
 
+from flask import Flask
 from slackeventsapi import SlackEventAdapter
 
-from utils import Broker, logger, broker_config
-from message_handlers import MessageHandlerFactory, HandlerType
+from utils import Broker, Publisher, broker_config, logger
+from handlers import MessageHandler
+from handlers.events import Event, EventHandler
+from handlers.clients import SlackClient
 
-parser = argparse.ArgumentParser(description="Slack bot configuration.")
-parser.add_argument("--topic", default="pair")
-parser.add_argument("--client_id", default="slackbot-pub")
-args = parser.parse_args()
+
+API_BASE = "/slack/events"
 
 app = Flask(__name__)
-slack_events_adapter = SlackEventAdapter(os.getenv("SLACK_EVENT_TOKEN"), "/slack/events", app)
-message_handlers = [
-    MessageHandlerFactory.create_handler(
-        _type=HandlerType.BINANCE, client_id=args.client_id, broker=Broker.from_dict(broker_config), topic=args.topic
-    )
-]
+slack_events_adapter = SlackEventAdapter(os.getenv("SLACK_EVENT_TOKEN"), API_BASE, app)
 
 
 @slack_events_adapter.on("message")
 def message(payload):
     try:
-        for handler in message_handlers:
-            handler.on_message(payload=payload)
+        event = Event.from_payload(payload)
+        for handler in handlers:
+            handler.on_event(event)
     except Exception as err:
-        logger.error("Slack OnMessage error: %s" % err)
+        logger.exception(err)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--topic", type=str, default="pair")
+    parser.add_argument("--client-id", type=str, default="slackbot-pub")
+    parser.add_argument("--host", type=str, default="0.0.0.0")
+    parser.add_argument("--port", type=int, default=5001)
 
-    app.run(host="0.0.0.0", port=5001)
+    args = parser.parse_args()
+    publisher = Publisher(args.client_id, Broker.from_dict(broker_config))
+    slack_client = SlackClient()
+    handlers: List[EventHandler] = [MessageHandler(publisher, slack_client, args.topic)]
+    app.run(host=args.host, port=args.port)
+    for handler in handlers:
+        handler.on_terminate()
